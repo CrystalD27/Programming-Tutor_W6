23d13185a5bc05f15c505611e17532c34aa8f02a
Crystal <ytgliniak@gmail.com>
2022-09-02 19:30:50 +0200

    Update 2022-09-02T17:30:48.479Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,9 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+$(document).ready(function () {
+  $('.btn__hambuger').on('click', function (e) {
+    $('.btn__hambuger').toggleClass('open');
+    $('.nav__item').toggleClass('open');
+  });
 });
 //# sourceMappingURL=all.js.map
