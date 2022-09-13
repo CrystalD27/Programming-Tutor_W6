b2c19728801aa7a0b4be3374b7f689bac19a990f
Crystal <ytgliniak@gmail.com>
2022-09-13 10:16:41 +0200

    update

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,9 +1,21 @@
 
-$(document).ready(() => {
+var swiper = new Swiper(".commentSwiper", {
 
-  $('.btn__hambuger').on('click', function (e) {
-    $('.btn__hambuger').toggleClass('open');
-    $('.nav__item').toggleClass('open');
-  })
+  slidesPerView: 1,
+  // 倒底會回第一張
+  loop: true,
+  // 有fade效果才不會整張切換
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 
-})
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
