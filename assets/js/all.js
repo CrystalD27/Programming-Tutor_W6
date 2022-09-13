267e23e62bb29412f1e8b563db0399b7cacbc2b4
Crystal <ytgliniak@gmail.com>
2022-09-13 10:17:20 +0200

    Update 2022-09-13T08:17:18.879Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,9 +1,21 @@
 "use strict";
 
-$(document).ready(function () {
-  $('.btn__hambuger').on('click', function (e) {
-    $('.btn__hambuger').toggleClass('open');
-    $('.nav__item').toggleClass('open');
-  });
+var swiper = new Swiper(".commentSwiper", {
+  slidesPerView: 1,
+  // 倒底會回第一張
+  loop: true,
+  // 有fade效果才不會整張切換
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  }
 });
 //# sourceMappingURL=all.js.map
